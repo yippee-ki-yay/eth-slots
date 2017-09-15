7c9edd7c69e11ce6019467e88ec6f4a596a62b7a
nenad <nesa993@gmail.com>
2017-09-15 16:33:34 +0200

    deploying the new updates

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 App = {
   web3Provider: null,
   contracts: {},
+  balance: 0,
   account: null,
   instance: null,
+  machine1: null,
+  machine2: null,
+  machine3: null,
+  started: 0,
   roll1: -1,
   roll2: -1,
   roll3: -1,
@@ -48,26 +53,41 @@ App = {
   },
 
   whitdraw: function() {
-    instance.withdraw.call().then(function(resp) {
-        console.log(resp);
-        App.checkBalance();
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+      if(App.balance != 0) {
+        instance.withdraw.sendTransaction({from: App.account, value: 0}).then(function(resp) {
+            console.log(resp);
+
+            setTimeout(App.checkBalance, 2000);
+ 
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+      } else {
+          alert("Win balance is zero no ether to withdraw");
+      }
   },
 
   checkAccount: function() {
     web3.eth.getAccounts(function(error, accounts) {
         App.account = accounts[0];
 
-        App.contracts.SlotMachine.at("0x03CCb0B8eb37271dee500C06CdBAf134f7f6b38B").then(function(_instance) {
+        App.contracts.SlotMachine.at("0x791D73721734De50d1EAeABF2C8dc4eB09de0547").then(function(_instance) {
             instance = _instance;
 
              var event = instance.Rolled();
 
              event.watch(function(err, resp) {
-                 console.log(resp);
+                 if(resp.event === "Rolled") {
+
+                     App.roll1 = resp.args.rand1.valueOf();
+                     App.roll2 = resp.args.rand2.valueOf();
+                     App.roll3 = resp.args.rand3.valueOf();
+
+                     console.log(App.roll1, App.roll2, App.roll3);
+
+                     setTimeout(App.checkBalance, 1000);
+                 }
              });
 
             App.checkBalance();
@@ -77,13 +97,11 @@ App = {
 
    checkBalance: function() {
 
-    console.log("Acc: " + App.account);
+    instance.balanceOf.call(App.account).then(function(_balance) {
 
-    instance.balanceOf.call(App.account).then(function(balance) {
+        App.balance = _balance.valueOf();
 
-        var balance = balance.valueOf();
-
-        var balanceInEther = web3.fromWei(balance, "ether");
+        var balanceInEther = web3.fromWei(App.balance, "ether");
 
         $("#balance").text(balanceInEther + " ether");
         
@@ -94,25 +112,18 @@ App = {
   startRoll: function() {
     event.preventDefault();
 
-    console.log("Roll was pressed");
+    if(App.started != 0) {
+        return;
+    }
 
-    App.contracts.SlotMachine.at("0x03CCb0B8eb37271dee500C06CdBAf134f7f6b38B").then(function(instance) {
+    App.contracts.SlotMachine.at("0x791D73721734De50d1EAeABF2C8dc4eB09de0547").then(function(instance) {
 
         return instance.oneRoll.sendTransaction({from: App.account, value: web3.toWei('0.1', 'ether')});
 
     }).then(function() {
+        App.startShuffle();
 
         return instance.getResult(App.account);
-    }).then(function(res) {
-        if(res.length >= 2) {
-            App.roll1 = res[0].valueOf();
-            App.roll2 = res[1].valueOf();
-            App.roll3 = res[2].valueOf();
-
-            console.log(App.roll1, App.roll2, App.roll3);
-        }
-
-        App.checkBalance();
     })
     .catch(function(err) {
     console.log(err.message);
@@ -121,61 +132,77 @@ App = {
 
   prizeWon: function() {
 
-    console.log("Prize won");
+    var msg = "";
 
-    if((App.roll1 == App.roll2) && (App.roll1 == App.roll3)) {
-        $("#header-msg").text("Congratulation you won!!!");
-    } else if ((App.roll1 == App.roll2) || (App.roll1 == App.roll3) || (App.roll2 == App.roll3)) {
-        $("#header-msg").text("Not bad, you live to try again");
+    if(App.roll1 == 5 && App.roll2 == 5 && App.roll3 == 5) {
+        msg = "Congratulation you won 3 ether";
+    } else if(App.roll1 == 6 && App.roll2 == 5 && App.roll3 == 6)  {
+        msg = "Congratulation you won 2 ether";
+    } else if(App.roll1 == 4 && App.roll2 == 4 && App.roll3 == 4)  {
+        msg = "Congratulation you won 1.5 ether";
+    } else if(App.roll1 == 3 && App.roll2 == 3 && App.roll3 == 3)  {
+        msg = "Congratulation you won 1.2 ether";
+    } else if(App.roll1 == 2 && App.roll2 == 2 && App.roll3 == 2)  {
+        msg = "Congratulation you won 1 ether";
+    } else if(App.roll1 == 1 && App.roll2 == 1 && App.roll3 == 1)  {
+        msg = "Congratulation you won 0.5 ether";
     } else {
-        $("#header-msg").text("Ouch, maybe another try?");
+        msg = "Better luck next time";
     }
+
+    $("#header-msg").text(msg);
+
+    App.checkBalance();
+
+    $("#slotMachineButtonShuffle").attr("disabled", false);
+  },
+
+  startShuffle: function() {
+    App.started = 3;
+    App.machine1.shuffle();
+    App.machine2.shuffle();
+    App.machine3.shuffle();
+
+    $("#slotMachineButtonShuffle").attr("disabled", true);
   },
 
   slotMachine: function() {
 
-      	var machine1 = $("#casino1").slotMachine({
+      	App.machine1 = $("#casino1").slotMachine({
             active	: 0,
             delay	: 500
         });
 
-        var machine2 = $("#casino2").slotMachine({
+        App.machine2 = $("#casino2").slotMachine({
             active	: 1,
             delay	: 500
         });
 
-        var machine3 = $("#casino3").slotMachine({
+        App.machine3 = $("#casino3").slotMachine({
             active	: 2,
             delay	: 500
         });
 
-        machine1.setRandomize(function() { return App.roll1; });
-        machine2.setRandomize(function() { return App.roll2; });
-        machine3.setRandomize(function() { return App.roll3; });
+        App.machine1.setRandomize(function() { return App.roll1; });
+        App.machine2.setRandomize(function() { return App.roll2; });
+        App.machine3.setRandomize(function() { return App.roll3; });
 
-        var started = 0;
-
-        $("#slotMachineButtonShuffle").click(function(){
-            started = 3;
-            machine1.shuffle();
-            machine2.shuffle();
-            machine3.shuffle();
-        });
+        App.started = 0;
 
         $("#slotMachineButtonStop").click(function(){
-            switch(started){
+            switch(App.started){
                 case 3:
-                    machine1.stop();
+                    App.machine1.stop();
                     break;
                 case 2:
-                    machine2.stop();
+                    App.machine2.stop();
                     break;
                 case 1:
-                    machine3.stop();
+                    App.machine3.stop();
                     App.prizeWon();
                     break;
             }
-            started--;
+            App.started--;
         });
   }
 
